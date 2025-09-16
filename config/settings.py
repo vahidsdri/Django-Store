23c47734b5109ace822ff2bfddf682917035ae10
Vahid sadri <vahidsadriv@gmail.com>
2025-09-16 16:19:58 +0330

    feat(models):add custom user model.

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'store',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
